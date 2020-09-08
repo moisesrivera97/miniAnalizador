d548ac9f4bcc6dde916803993044a74f8bfba19a
moises97 <moises.rivera@alumnos.udg.mx>
2020-09-08 12:39:54 -0500

    Se crea interfaz gráfica

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.botonAnalizar = new System.Windows.Forms.Button();
+            this.TextBoxEntrada = new System.Windows.Forms.TextBox();
+            this.LabelEntrada = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // botonAnalizar
+            // 
+            this.botonAnalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonAnalizar.Location = new System.Drawing.Point(619, 99);
+            this.botonAnalizar.Name = "botonAnalizar";
+            this.botonAnalizar.Size = new System.Drawing.Size(118, 47);
+            this.botonAnalizar.TabIndex = 0;
+            this.botonAnalizar.Text = "Analizar";
+            this.botonAnalizar.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxEntrada
+            // 
+            this.TextBoxEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxEntrada.Location = new System.Drawing.Point(12, 52);
+            this.TextBoxEntrada.Name = "TextBoxEntrada";
+            this.TextBoxEntrada.Size = new System.Drawing.Size(725, 29);
+            this.TextBoxEntrada.TabIndex = 1;
+            // 
+            // LabelEntrada
+            // 
+            this.LabelEntrada.AutoSize = true;
+            this.LabelEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelEntrada.Location = new System.Drawing.Point(348, 9);
+            this.LabelEntrada.Name = "LabelEntrada";
+            this.LabelEntrada.Size = new System.Drawing.Size(88, 26);
+            this.LabelEntrada.TabIndex = 2;
+            this.LabelEntrada.Text = "Entrada";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(749, 158);
+            this.Controls.Add(this.LabelEntrada);
+            this.Controls.Add(this.TextBoxEntrada);
+            this.Controls.Add(this.botonAnalizar);
             this.Name = "Form1";
             this.Text = "Analizador Lexico";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button botonAnalizar;
+        private System.Windows.Forms.TextBox TextBoxEntrada;
+        private System.Windows.Forms.Label LabelEntrada;
     }
 }
 
