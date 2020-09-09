0b0af437fea2f5c02d396a8ae6d1d3469a0c734e
moises97 <moises.rivera@alumnos.udg.mx>
2020-09-09 14:26:00 -0500

    Se añade la clase token

--- AnalizadorLexico.cs
+++ AnalizadorLexico.cs
@@ -8,5 +8,25 @@ namespace MiniAnalizadorLexico
 {
     class AnalizadorLexico
     {
+        private List<Token> salida;
+        private int estado;
+        private string auxLex;
+
+        public List<Token> escanear(string entrada)
+        {
+            entrada = entrada + "#";
+            salida = new List<Token>();
+            estado = 0;
+            auxLex = "";
+            char c;
+
+            return salida;
+        }
+
+        private void addToken(Token.Tipo tipo)
+        {
+            salida.Add(new Token(tipo, auxLex));
+            auxLex = "";
+        }
     }
 }
---
+++ Token.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAnalizadorLexico
+{
+    class Token
+    {
+        public enum Tipo
+        {
+            REAL,
+            IDENTIFICADOR,
+            SIMBOLO_ASIGNACION
+        }
+
+        private Tipo tipoToken;
+        private string valor;
+
+        public Token(Tipo tipo, string auxLex)
+        {
+            this.tipoToken = tipo;
+            this.valor = auxLex;
+        }
+
+        public Tipo getTipo()
+        {
+            return tipoToken;
+        }
+        public string getValor()
+        {
+            return valor;
+        }
+    }
+}
